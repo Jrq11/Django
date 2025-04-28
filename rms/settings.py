0e2c3fe1eeb02f45a794978300e893156b055111
Jan Red <janredquierra@mabinicolleges.edu.ph>
2025-04-28 20:00:38 +0800

    Ready for Railway deployment

--- rms/settings.py
+++ rms/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c3+tz%97ac*8eya&-=c5!dzdt!i#$&%0d%bps-+gu!wm9h66(#'
+#SECRET_KEY = 'django-insecure-c3+tz%97ac*8eya&-=c5!dzdt!i#$&%0d%bps-+gu!wm9h66(#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-c3+tz%97ac*8eya&-=c5!dzdt!i#$&%0d%bps-+gu!wm9h66(#')
+DEBUG = False
 
-DEBUG = True
-
-ALLOWED_HOSTS = ['192.168.1.7', '127.0.0.1', '192.168.1.5']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
