31635ac9e348e3fcf6ede085c1b4cf4dca61cc09
Jan Red <janredquierra@mabinicolleges.edu.ph>
2025-05-16 23:40:32 +0800

    Render ready

--- rms/settings.py
+++ rms/settings.py
@@ -91,14 +91,9 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')  # Works locally or in Render
+    )
 }
 
 
@@ -164,8 +159,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apocompound@gmail.com'
-EMAIL_HOST_PASSWORD = 'xhlvinvtfguslmzk'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-OTP_EMAIL_SECRET_KEY = 'your-secret-key'
+OTP_EMAIL_SECRET_KEY = config('OTP_EMAIL_SECRET_KEY')
